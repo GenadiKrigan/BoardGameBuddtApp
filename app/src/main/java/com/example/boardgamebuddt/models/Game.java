780682e0cf37791c96029ef1a94b54edd13235d0
Genadi <genadikrigan97@gmail.com>
2026-02-14 12:36:43 +0200

    Updated Game data model and created Add Game fragment layout

--- app/src/main/java/com/example/boardgamebuddt/models/Game.java
+++ app/src/main/java/com/example/boardgamebuddt/models/Game.java
@@ -1,91 +1,49 @@
 package com.example.boardgamebuddt.models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Game {
     private String firebaseId;
     private String name;
     private int minPlayers;
     private int maxPlayers;
+    private int bestPlayers;
     private int avgPlayTime;
     private int difficulty;
-    private String category;
+    private List<String> categories;
     private String learningStatus;
-    private boolean isBorrowed;
     private boolean isUnplayed;
     private int totalPlayTime;
     private int playCount;
     private String rulesNote;
 
     public Game(){
+        this.categories = new ArrayList<>();
     }
 
-    public Game(String firebaseId, String name, int minPlayers, int maxPlayers, int avgPlayTime, int difficulty, String category){
+    public Game(String firebaseId, String name, int minPlayers, int maxPlayers, int bestPlayers, int avgPlayTime, int difficulty, List<String> categories, boolean alreadyPlayed){
         this.firebaseId = firebaseId;
         this.name = name;
         this.minPlayers = minPlayers;
         this.maxPlayers = maxPlayers;
+        this.bestPlayers = bestPlayers;
         this.avgPlayTime = avgPlayTime;
         this.difficulty = difficulty;
-        this.category = category;
-        this.learningStatus = "Need to Learn";
-        this.isBorrowed = false;
-        this.isUnplayed = true;
-        this.totalPlayTime = 0;
+        this.categories = categories != null ? categories : new ArrayList<>();
+        if(alreadyPlayed){
+            this.learningStatus = "Learn";
+            this.isUnplayed = false;
+        }
+        else {
+            this.learningStatus = "Need to learn";
+            this.isUnplayed = true;
+        }
         this.playCount = 0;
+        this.totalPlayTime = 0;
         this.rulesNote = "";
-    }
-    //setters
-    public void setFirebaseId(String firebaseId) {
-        this.firebaseId = firebaseId;
-    }
 
-    public void setName(String name) {
-        this.name = name;
     }
-
-    public void setMinPlayers(int minPlayers) {
-        this.minPlayers = minPlayers;
-    }
-
-    public void setMaxPlayers(int maxPlayers) {
-        this.maxPlayers = maxPlayers;
-    }
-
-    public void setAvgPlayTime(int avgPlayTime) {
-        this.avgPlayTime = avgPlayTime;
-    }
-
-    public void setDifficulty(int difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setLearningStatus(String learningStatus) {
-        this.learningStatus = learningStatus;
-    }
-
-    public void setBorrowed(boolean borrowed) {
-        isBorrowed = borrowed;
-    }
-
-    public void setUnplayed(boolean unplayed) {
-        isUnplayed = unplayed;
-    }
-
-    public void setTotalPlayTime(int totalPlayTime) {
-        this.totalPlayTime = totalPlayTime;
-    }
-
-    public void setPlayCount(int playCount) {
-        this.playCount = playCount;
-    }
-
-    public void setRulesNote(String rulesNote) {
-        this.rulesNote = rulesNote;
-    }
-    //Getters
 
     public String getFirebaseId() {
         return firebaseId;
@@ -103,6 +61,10 @@ public class Game {
         return maxPlayers;
     }
 
+    public int getBestPlayers() {
+        return bestPlayers;
+    }
+
     public int getAvgPlayTime() {
         return avgPlayTime;
     }
@@ -111,16 +73,12 @@ public class Game {
         return difficulty;
     }
 
-    public String getCategory() {
-        return category;
+    public List<String> getCategories() {
+        return categories;
     }
 
     public String getLearningStatus() {
         return learningStatus;
-    }
-
-    public boolean isBorrowed() {
-        return isBorrowed;
     }
 
     public boolean isUnplayed() {
@@ -137,5 +95,57 @@ public class Game {
 
     public String getRulesNote() {
         return rulesNote;
+    }
+
+    public void setFirebaseId(String firebaseId) {
+        this.firebaseId = firebaseId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public void setBestPlayers(int bestPlayers) {
+        this.bestPlayers = bestPlayers;
+    }
+
+    public void setAvgPlayTime(int avgPlayTime) {
+        this.avgPlayTime = avgPlayTime;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public void setLearningStatus(String learningStatus) {
+        this.learningStatus = learningStatus;
+    }
+
+    public void setUnplayed(boolean unplayed) {
+        isUnplayed = unplayed;
+    }
+
+    public void setTotalPlayTime(int totalPlayTime) {
+        this.totalPlayTime = totalPlayTime;
+    }
+
+    public void setPlayCount(int playCount) {
+        this.playCount = playCount;
+    }
+
+    public void setRulesNote(String rulesNote) {
+        this.rulesNote = rulesNote;
     }
 }
